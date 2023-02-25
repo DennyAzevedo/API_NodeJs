e109f2fa2c21ad0b447b9007bb081593b2f11a45
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-02-25 12:55:56 -0300

    Projeto iniciado - Exemplo de uso terminado

---
+++ backend/src/router.js
@@ -0,0 +1,10 @@
+//const { Router } = require('express');
+import { Router } from 'express';
+import SessionController from './controllers/SessionController';
+
+const routes = new Router();
+
+routes.post('/sessions', SessionController.store);
+
+//module.exports = routes;
+export default routes;
