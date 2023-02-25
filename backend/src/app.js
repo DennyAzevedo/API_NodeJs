972a0a5c255e5845fa25738610cd02f5c4fd07fb
Denny Azevedo <denny.filho@animaeducacao.com.br>
2023-02-25 17:51:48 -0300

    HouseController.store - Upload imagens

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@
 //const routes = require('./router');
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
 import routes from './router';
 
 class App{
@@ -17,6 +18,10 @@ class App{
 	}
 
 	middlewares() {
+		this.server.use(
+			'/files',
+			express.static(path.resolve(__dirname, '..', 'uploads')),
+		);
 		this.server.use(express.json());
 	}
 
